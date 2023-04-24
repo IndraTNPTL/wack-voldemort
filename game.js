5d58052d4c031e9a07fdff8b11e4249c27927bad
Lopoticha_972 <126521804+IndraTNPTL@users.noreply.github.com>
2023-04-24 10:06:48 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,16 @@
+// const data = require("./data");
+
+// Automatic set up the grid of the game board for html
+window.onload = function () {
+	setGame();
+};
+
+function setGame() {
+	for (let i = 0; i < 9; i++) {
+		let tileHole = document.createElement("div");
+		// Giving the tiles an unique ID
+		tileHole.id = "tile " + i.toString();
+		// Inserting the tiles in the HTML
+		document.getElementById("gameboard").appendChild(tileHole);
+	}
+}
