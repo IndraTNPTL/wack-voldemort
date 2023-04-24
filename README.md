5295072c45500091595d8c69019de6c0a68d3a47
Indra Tinot-Patole <indra.tinot.patole@gmail.com>
2023-04-24 15:58:31 +0200

    HTML + basic CSS, tried to make it responsive from the beginning. Will see if I managed after

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # wack-voldemort
+
 Globally a wack a mole game but with Voldemort head XD
+
+As a proud and loyal Hufflepuff, take revenge for Cedric Diggory's death during the Goblet of Fire competition and wack Voldemort!
+
+<!-- Repository link -->
+
+https://github.com/IndraTNPTL/wack-voldemort
+
+<!-- Hogwart house colors + font if needed -->
+
+https://codepen.io/Jill-Lambert/pen/bwevpB
+https://www.color-hex.com/color-palette/816
+
+<!-- Button CSS inspo -->
+
+https://cssgradient.io/gradient-backgrounds/
+
+<!-- Game from class inspo -->
+
+https://github.com/WebDev-Paris-104/W2D4/blob/main/style.css
+
+<!-- Help game -->
+
+https://www.aldohadinata.com/how-to-make-a-simple-whack-a-mole-game-in-javascript/
+
+<!-- height: calc(80vh - 5rem);
+aspect-ratio: 9/16; -->
--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 // const data = require("./data");
 
 // Automatic set up the grid of the game board for html
-window.onload = function () {
-	setGame();
-};
+// window.onload = function () {
+// 	setGame();
+// };
 
-function setGame() {
-	for (let i = 0; i < 9; i++) {
-		let tileHole = document.createElement("div");
-		// Giving the tiles an unique ID
-		tileHole.id = "tile " + i.toString();
-		// Inserting the tiles in the HTML
-		document.getElementById("gameboard").appendChild(tileHole);
-	}
-}
+// function setGame() {
+// 	for (let i = 0; i < 9; i++) {
+// 		let tileHole = document.createElement("div");
+// 		// Giving the tiles an unique ID
+// 		tileHole.id = "tile " + i.toString();
+// 		// Inserting the tiles in the HTML
+// 		document.getElementById("gameboard").appendChild(tileHole);
+// 	}
+// }
+
+function startGame() {}
